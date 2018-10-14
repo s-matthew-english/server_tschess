667fcfea7f313d61f9f19d792c4650ae66838024
tschess <s.matthew.english@googlemail.com>
2018-10-14 18:47:28 -0400

    transmit timer broadcast

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -1,12 +1,14 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ClientHandler extends Thread {
     private Socket socket;
-    DataOutputStream dataOutputStream;
 
     ClientHandler(Socket clientSocket) {
         this.socket = clientSocket;
@@ -16,6 +18,7 @@ public class ClientHandler extends Thread {
     public void run() {
         InputStream inputStream;
         BufferedReader bufferedReader;
+        DataOutputStream dataOutputStream;
 
         try {
             inputStream = socket.getInputStream();
@@ -27,6 +30,23 @@ public class ClientHandler extends Thread {
                 Server.gameState = clientInputMessage;
                 System.out.println(Server.gameState);
             }
+
+            // says "foo" every half second
+            Timer timer = new Timer();
+            timer.scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("transmitting message...");
+                        dataOutputStream.writeBytes(Server.gameState + "\n");
+                        dataOutputStream.flush();
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, 0, 500);
+
         } catch (Exception e) {
             System.out.println("error");
         }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,12 +1,9 @@
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 // Server class
 public class Server {
     static String gameState;
-    public static List<ClientHandler> connectedClients = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket = new ServerSocket(4444);
@@ -19,18 +16,7 @@ public class Server {
                 System.out.println("A new client is connected : " + socket);
 
                 ClientHandler client = new ClientHandler(socket);
-
-                if (!connectedClients.contains(client)) {
-                    System.out.println("Assigning new thread for this client");
-                    connectedClients.add(client);
-                    System.out.println("size: " + connectedClients.size());
-                }
                 client.start();
-
-                for (ClientHandler activeClient : connectedClients) {
-                    activeClient.dataOutputStream.writeBytes(Server.gameState + "\n");
-                    activeClient.dataOutputStream.flush();
-                }
 
             } catch (Exception e) {
                 System.out.println("error!");

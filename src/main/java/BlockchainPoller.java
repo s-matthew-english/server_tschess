7fb83e11cb8040001ea5c19c46c8e85fe81bdd59
tschess <s.matthew.english@googlemail.com>
2018-11-02 17:08:07 -0400

    chron job working

--- src/main/java/BlockchainPoller.java
+++ src/main/java/BlockchainPoller.java
@@ -5,30 +5,24 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import java.io.IOException;
+import java.util.TimerTask;
 
-public class BlockchainPoller {
+public class BlockchainPoller extends TimerTask {
+    @Override
+    public void run() {
+        MediaType json = MediaType.parse("application/json; charset=utf-8");
+        String content = "{\"jsonrpc\":\"2.0\",\"id\":67,\"method\":\"web3_clientVersion\"}";
+        RequestBody body = RequestBody.create(json, content);
 
-    public static void hitInfura() {
+        String url = "https://mainnet.infura.io";
+        Request request = new Request.Builder().post(body).url(url).build();
 
-        Runnable myRunnable =
-                () -> {
-                    MediaType json = MediaType.parse("application/json; charset=utf-8");
-                    String content = "{\"jsonrpc\":\"2.0\",\"id\":67,\"method\":\"web3_clientVersion\"}";
-                    RequestBody body = RequestBody.create(json, content);
-
-                    String url = "https://mainnet.infura.io";
-                    Request request = new Request.Builder().post(body).url(url).build();
-
-                    OkHttpClient client = new OkHttpClient();
-                    try {
-                        Response resp = client.newCall(request).execute();
-                        System.out.println(resp.body().string());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                };
-
-        myRunnable.run();
-        myRunnable.run();
+        OkHttpClient client = new OkHttpClient();
+        try {
+            Response resp = client.newCall(request).execute();
+            System.out.println(resp.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/BlockchainPollerTest.java
+++ src/test/java/BlockchainPollerTest.java
@@ -1,13 +1,16 @@
 import org.junit.Test;
 
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
 public class BlockchainPollerTest {
 
     @Test
-    public void hitInfuraTest() {
-        try {
-            BlockchainPoller.hitInfura();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void hitInfuraTest() throws Exception {
+        Timer t = new Timer();
+        BlockchainPoller mTask = new BlockchainPoller();
+        t.scheduleAtFixedRate(mTask, 0, 10);
+
+        TimeUnit.SECONDS.sleep(1000);
     }
 }
